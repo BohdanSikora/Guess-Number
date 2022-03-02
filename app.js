125143b02ed9fb2b4193f3ec73b56d0e6dd976da
Bohdan Sikora <bohdansikora@Bohdans-MacBook-Air.local>
2022-03-02 18:05:23 +0200

    second commit

--- app.js
+++ app.js
@@ -15,18 +15,19 @@
 // }
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value)
 console.log(guess, typeof guess)
  
-if (!guess) {
+if (!guess) { 
      document.querySelector('.message').textContent = '👺 No number!';
      // when ther is not input 
  } else if (guess === secretNumber) {
     document.querySelector('.message').textContent ='))), Correct Number!'
-document.querySelector('body').style.backgroundColor = '#68b37';
-document.querySelector('.number').style.width = '38rem';
+    document.querySelector('.message').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#608b37';
+    document.querySelector('.number').style.width = '38rem';
 
  } else if (guess > secretNumber) {
      if (score > 1) {
